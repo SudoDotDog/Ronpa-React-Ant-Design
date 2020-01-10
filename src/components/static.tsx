1c28dbf08282c0a18bbed7ca7574c5d2865653ce
WMXPY <wm@mengw.io>
2020-01-10 11:19:48 -0600

    add drag hover

--- src/components/static.tsx
+++ src/components/static.tsx
@@ -4,8 +4,9 @@
  * @description Static Editor
  */
 
-import { mergeClasses } from "@sudoo/jss";
-import { Button, Comment, Input } from "antd";
+import { assertIfTrue, mergeClasses } from "@sudoo/jss";
+import { Button, Comment, Icon, Input } from "antd";
+import { Classes } from "jss";
 import * as React from "react";
 import { editorStyle } from "../style/editor";
 
@@ -18,11 +19,14 @@ export type RonpaStaticEditorProps = {
 
     readonly getAvatar?: (author: string) => string | React.ReactNode;
     readonly onSubmit?: (content: string) => void;
+
+    readonly allowFile?: boolean;
 };
 
 export type RonpaStaticEditorStates = {
 
     readonly content: string;
+    readonly dragHover: boolean;
 };
 
 export class RonpaStaticEditor extends React.Component<RonpaStaticEditorProps, RonpaStaticEditorStates> {
@@ -30,9 +34,10 @@ export class RonpaStaticEditor extends React.Component<RonpaStaticEditorProps, R
     public readonly state: RonpaStaticEditorStates = {
 
         content: '',
+        dragHover: false,
     };
 
-    private readonly _editorStyle = editorStyle.use();
+    private readonly _editorStyle: Classes = editorStyle.use();
 
     public constructor(props: RonpaStaticEditorProps) {
 
@@ -51,16 +56,7 @@ export class RonpaStaticEditor extends React.Component<RonpaStaticEditorProps, R
             style={this.props.style}
             avatar={this._getAvatar(this.props.username)}
             content={<div>
-                <Input.TextArea
-                    value={this.state.content}
-                    autoSize={{
-                        minRows: 2,
-                        maxRows: 6,
-                    }}
-                    onChange={(value: React.ChangeEvent<HTMLTextAreaElement>) => this.setState({
-                        content: value.target.value,
-                    })}
-                />
+                {this.props.allowFile ? this._renderFileTextArea() : this._renderBasicTextArea()}
                 <Button
                     className={this._editorStyle.submitButton}
                     type="primary"
@@ -68,6 +64,53 @@ export class RonpaStaticEditor extends React.Component<RonpaStaticEditorProps, R
                 >Submit</Button>
             </div>}
         />);
+    }
+
+    private _renderBasicTextArea() {
+
+        return (<Input.TextArea
+            value={this.state.content}
+            autoSize={{
+                minRows: 2,
+                maxRows: 6,
+            }}
+            className={mergeClasses(
+                assertIfTrue(this.props.allowFile, this._editorStyle.textAreaWithDrop),
+            )}
+            onChange={(value: React.ChangeEvent<HTMLTextAreaElement>) => this.setState({
+                content: value.target.value,
+            })}
+        />);
+    }
+
+    private _renderFileTextArea() {
+
+        return (<div>
+            <div>
+                <Input.TextArea
+                    value={this.state.content}
+                    autoSize={{
+                        minRows: 2,
+                        maxRows: 6,
+                    }}
+                    className={mergeClasses(
+                        this._editorStyle.textAreaWithDrop,
+                    )}
+                    onChange={(value: React.ChangeEvent<HTMLTextAreaElement>) => this.setState({
+                        content: value.target.value,
+                    })}
+                />
+                <Button
+                    type="ghost"
+                    block
+                    className={mergeClasses(
+                        this._editorStyle.uploadIndicator,
+                    )}>
+                    <Icon type="paper-clip" />
+                    Drag or Click
+                </Button>
+            </div>
+        </div>);
     }
 
     private _submitChange() {
--- src/style/editor.ts
+++ src/style/editor.ts
@@ -23,6 +23,16 @@ const editorStyleBase: JSSStyle = {
     submitButton: {
         marginTop: '5px',
     },
+    textAreaWithDrop: {
+        borderBottomLeftRadius: "0 !important",
+        borderBottomRightRadius: "0 !important",
+    },
+    uploadIndicator: {
+        borderTop: '0px !important',
+        borderTopLeftRadius: "0 !important",
+        borderTopRightRadius: "0 !important",
+        textAlign: 'left !important',
+    },
 };
 
 export const editorStyle: StyleManager = StyleManager.create(editorStyleBase, 'Editor').setPrefix('Ronpa-React-Ant-Design-');
