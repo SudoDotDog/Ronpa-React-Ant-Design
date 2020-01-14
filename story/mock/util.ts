e19356d646d5cf1be1e7b6685e1665f20c41ff50
WMXPY <wm@mengw.io>
2020-01-14 14:03:00 -0600

    use delay upload

---
+++ story/mock/util.ts
@@ -0,0 +1,23 @@
+/**
+ * @author WMXPY
+ * @namespace React_Ant_Design
+ * @description Util
+ * @override Mock
+ */
+
+import * as Chance from "chance";
+import { RonpaEditorUploadResult } from "../../src/editor/type";
+
+export const mockUploadFunction = async (file: File): Promise<RonpaEditorUploadResult> => {
+
+    const chance: Chance.Chance = new Chance('mock-ronpa');
+
+    await new Promise((resolve: () => void) => setTimeout(resolve, chance.natural({
+        max: 3000,
+    })));
+
+    return {
+        path: 'https://google.com/robots.txt',
+        uploadedAt: new Date(),
+    } as RonpaEditorUploadResult;
+};
