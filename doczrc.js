2b246cb4004d96ea15547087bfaadd4418d1e7d2
WMXPY <wm@mengw.io>
2020-03-20 12:06:23 -0500

    Revert "[chore] attempt to upgrade docz"
    This reverts commit 03fa25a13932277e90104b7722e5c83d553b2586.

--- doczrc.js
+++ doczrc.js
@@ -6,6 +6,16 @@
 
 export default {
     title: 'Ronpa React Ant Design',
-    files: '**/*.{md,markdown,mdx}',
+    codeSandbox: false,
     typescript: true,
+    modifyBundlerConfig: (bundlerConfig) => {
+        const rules = [
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            }
+        ];
+        bundlerConfig.module.rules.push(...rules);
+        return bundlerConfig;
+    },
 };
