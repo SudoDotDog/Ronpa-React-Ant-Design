1a0392557bf9eb72a315815064f3c4944668c9a8
WMXPY <wm@mengw.io>
2020-03-20 12:29:32 -0500

    ignore

--- doczrc.js
+++ doczrc.js
@@ -8,24 +8,16 @@ export default {
     title: 'Ronpa React Ant Design',
     codeSandbox: false,
     typescript: true,
-    modifyBundlerConfig: (bundlerConfig) => {
-        const rules = [
-            {
-                test: /\.css$/,
-                loader: "style-loader!css-loader"
-            }
-        ];
-        bundlerConfig.module.rules.push(...rules);
-        return bundlerConfig;
-    },
-    modifyBabelRc: babelrc => {
+    modifyBabelRc: (babelrc) => {
         const newBabelRc = {
             ...babelrc,
             plugins: [
-                ...babelrc.plugins,
                 "@babel/plugin-proposal-numeric-separator",
+                ...babelrc.plugins,
             ],
         };
+
+        console.log(newBabelRc);
         return newBabelRc;
     },
 };
